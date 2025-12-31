e1df2e515fc6d81a374404d39957d7c0ef8ea165
zohra <zohrakaberi.ftn@gmail.com>
2025-12-31 15:07:25 +0600

    final

--- ClassBuddy/src/main/java/com/classbuddy/controller/ManageStudentsController.java
+++ ClassBuddy/src/main/java/com/classbuddy/controller/ManageStudentsController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx. scene.Scene;
 import javafx.scene. control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import com.classbuddy.model. Classroom;
